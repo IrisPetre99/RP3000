f4409a9dc44bb3f7acffdf2753d9ba86f75bfb07
Iris Petre <I.A.Petre@student.tudelft.nl>
2025-05-14 13:20:33 +0200

    rebased main onto occlusio

--- exporters/kitti_exporter.py
+++ exporters/kitti_exporter.py
@@ -10,16 +10,31 @@ class KITTIExporter(Exporter):
         img1_rgb = qimage_to_rgb(img1)
         img2_rgb = qimage_to_rgb(img2)
 
+        # Cropping to KITTI resolution
+        kitti_width, kitti_height = 1242, 375
+
+        # Get original size
+        h, w, _ = img1_rgb.shape
+
+        # Compute cropping origin (center crop)
+        x_start = max(0, (w - kitti_width) // 2)
+        y_start = max(0, (h - kitti_height) // 2)
+
+        # Perform crop
+        img1_rgb = img1_rgb[y_start:y_start + kitti_height, x_start:x_start + kitti_width]
+        img2_rgb = img2_rgb[y_start:y_start + kitti_height, x_start:x_start + kitti_width]
+
+        # Determine next available export index
         files = os.listdir(os.path.join(export_dir, "image_2"))
         largest_index = 0
-        pattern=re.compile(r'^(\d{6})_\d{2}\.png$')
+        pattern = re.compile(r'^(\d{6})_\d{2}\.png$')
         for filename in files:
             match = pattern.match(filename)
             if match:
                 index = int(match.group(1))
                 largest_index = max(largest_index, index)
 
-        idx = f"{largest_index+1:06d}"
+        idx = f"{largest_index + 1:06d}"
 
         os.makedirs(os.path.join(export_dir, "flow_occ"), exist_ok=True)
         os.makedirs(os.path.join(export_dir, "image_2"), exist_ok=True)
@@ -27,14 +42,18 @@ class KITTIExporter(Exporter):
         cv2.imwrite(os.path.join(export_dir, "image_2", f"{idx}_10.png"), cv2.cvtColor(img1_rgb, cv2.COLOR_RGB2BGR))
         cv2.imwrite(os.path.join(export_dir, "image_2", f"{idx}_11.png"), cv2.cvtColor(img2_rgb, cv2.COLOR_RGB2BGR))
 
-        flow_h = img1.height()
-        flow_w = img1.width()
+        flow_h, flow_w, _ = img1_rgb.shape
         flow_img = np.zeros((flow_h, flow_w, 3), dtype=np.uint16)
-
         for ((x1, y1), (x2, y2)) in annotations:
-            x1_int, y1_int = int(round(x1)), int(round(y1))
-            dx = x2 - x1
-            dy = y2 - y1
+            x1_cropped = x1 - x_start
+            y1_cropped = y1 - y_start
+            x2_cropped = x2 - x_start
+            y2_cropped = y2 - y_start
+
+            x1_int = int(round(x1_cropped))
+            y1_int = int(round(y1_cropped))
+            dx = x2_cropped - x1_cropped
+            dy = y2_cropped - y1_cropped
 
             fx = int((dx * 64.0) + 2**15)
             fy = int((dy * 64.0) + 2**15)
